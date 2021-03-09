f7cf16309b55ebf0b0cf741e70fff877274e8e0d
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-03-09 16:48:22 -0300

    added the player asset and collisions

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-	
+	int currentScene;
 }
