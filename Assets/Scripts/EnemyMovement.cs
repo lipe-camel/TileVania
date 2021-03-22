a3c5a3ff385050ef0d40ed34f6ab3ad63d56c6ad
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-03-22 12:44:12 -0300

    refactored enemy1 code

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,61 +1,35 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
     //CONFIG PARAMS
     [SerializeField] float moveSpeed = 10f;
 
-    //STATS
-    bool haveGroundAhead;
-
     //CACHED REFERENCES
     Rigidbody2D rigidBody2D;
-    BoxCollider2D groundChecker;
-
-    //STRING REFERENCES
-    static string GROUND_LAYER = "Ground";
 
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
-        groundChecker = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
-        ManageMovement();
-        ManageBools();
-        Debug.Log(haveGroundAhead); 
+        Move();
     }
 
-    private void ManageBools()
-    {
-        haveGroundAhead = groundChecker.IsTouchingLayers(LayerMask.GetMask(GROUND_LAYER));
-    }
-
-
-    private void ManageMovement()
-    {
-        if (haveGroundAhead)
-        {
-            Move(1);
-        }
-        else
-        {
-            Move(-1);
-        }
-    }
-
-    private void Move(int direction)
-    {
-        transform.localScale = new Vector2(direction, 1f);
-        rigidBody2D.velocity = new Vector2(moveSpeed * direction, rigidBody2D.velocity.y);
-    }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        Turn();
+    }
+
+    private void Move()
+    {
+        rigidBody2D.velocity = new Vector2(moveSpeed * transform.localScale.x, rigidBody2D.velocity.y);
+    }
+
+    private void Turn()
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(rigidBody2D.velocity.x)), 1f);
     }
 }
